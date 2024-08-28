6e1c94534dc0b15996bbab34c58d2833edab4b06
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2024-08-28 14:14:32 +0200

    responsive

--- components/navbar.js
+++ components/navbar.js
@@ -7,14 +7,14 @@ export const Navbar = ({theme}) => {
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">ProjectAD</a>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal">
+        <div className="flex-none pr-2">
+          <ul className="menu menu-horizontal text-sm">
             <li tabIndex={0}>
               <a>
                 ACTIVIDADES  
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
               </a>
-              <ul className="p-2 bg-neutral text-neutral-content z-10">
+              <ul className="p-1 bg-neutral text-sm text-neutral-content z-10">
                 <li><a>AIRE</a></li>
                 <li><a>MONTANYA</a></li>
                 <li><a>AGUA</a></li>
