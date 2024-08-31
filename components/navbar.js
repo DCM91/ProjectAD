c07b97de2009a34187e19580596b53e7789f3d2b
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2024-08-31 14:40:15 +0200

    navbar mejorada

--- components/navbar.js
+++ components/navbar.js
@@ -3,43 +3,87 @@ import { Selector } from "./selector";
 
 export const Navbar = ({ theme }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+    if (isProfileOpen) setIsProfileOpen(false); // Cierra el otro menú si está abierto
+  };
+
+  const toggleProfile = () => {
+    setIsProfileOpen(!isProfileOpen);
+    if (isMenuOpen) setIsMenuOpen(false); // Cierra el otro menú si está abierto
   };
 
   return (
-    <div data-theme={theme} className="w-full navbar bg-neutral text-neutral-content">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">ProjectAD</a>
+    <div data-theme={theme} className="navbar bg-neutral text-neutral-content">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle"
+            onClick={toggleMenu}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          {isMenuOpen && (
+            <ul
+              className="menu menu-sm dropdown-content bg-neutral text-neutral-content rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li><a>Actividades</a></li>
+              <li><a>Packs</a></li>
+              <li><a><Selector /></a></li>
+            </ul>
+          )}
+        </div>
       </div>
-      <div className="flex-none pr-2">
-        <ul className="menu menu-horizontal text-sm ">
-          <li tabIndex={0} onClick={toggleMenu}>
-            <a>
-              Actividades
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            {isMenuOpen && (
-              <ul className="p-1 bg-neutral text-sm text-neutral-content text-end z-10">
-                <li><a>Aire</a></li>
-                <li><a>Montaña</a></li>
-                <li><a>Agua</a></li>
-                <li><a>Nieve</a></li>
-                <li><a>Motor</a></li>
-              </ul>
-            )}
-          </li>
-        </ul>
-        <Selector />
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl">Project AD</a>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+            onClick={toggleProfile}
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          {isProfileOpen && (
+            <ul
+              className="menu menu-sm dropdown-content bg-neutral text-neutral-content rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
