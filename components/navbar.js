5137cd0f020b50fde967ca98766767c121d6b71f
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2024-08-31 14:49:22 +0200

    cucadas

--- components/navbar.js
+++ components/navbar.js
@@ -65,7 +65,7 @@ export const Navbar = ({ theme }) => {
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                src="/assets/TTs.jpg"
               />
             </div>
           </div>
