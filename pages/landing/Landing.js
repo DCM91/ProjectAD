9037ab86ca1ef7f4d222ae0502dc74e2ba3d29a8
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2025-11-29 18:08:53 +0100

    prueba movil

--- pages/landing/Landing.js
+++ pages/landing/Landing.js
@@ -29,7 +29,62 @@ export const Landing = ({ theme }) => {
       </div>
 
       <h1 className="text-center h-80" >CONTENT LLAMATIVO</h1>
-      <h1 className="text-center h-80" >CONTENT LLAMATIVO</h1>
+      <div class="carousel carousel-center bg-transparent max-w-screen space-x-4">
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      class="rounded-box" />
+  </div>  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      class="rounded-box" />
+  </div>
+  <div class="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      class="rounded-box" />
+  </div>
+</div>
       </div>
       <div>
         <h1 className="text-center text-3xl text-warning pb-4" id="servicios">Servicios</h1>
