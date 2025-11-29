0896a24ce05735168d487e3f8be4e728f9ba8c9e
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2025-11-29 17:32:12 +0100

    svg prueba

--- components/Layout.js
+++ components/Layout.js
@@ -22,7 +22,7 @@ export const Layout = ({ title = "Byphnix" , children}) => {
             <title>{title}</title>
             <meta name="description" content="Byphnix" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/next.svg" />
         </Head>
 
         <header className='w-full bg-transparent ' >
