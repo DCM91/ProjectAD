a9ada79f6b725edb884d17442e3c622f04052e33
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2025-12-20 17:37:00 +0100

    z

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
     locales:["en", "es", "fr"],
     defaultLocale: "en",
     localeDetection: true, //detecta el local preferido por el user en el navegador(esta en true por defecto)
-  },
-  transpilePackages: ['framer-motion', 'yet-another-react-lightbox'],
+  }
 }
 
 module.exports = nextConfig
